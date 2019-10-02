d78d842146cecebef1d1f548ab439c2f823edaeb
mackacavs <mackenziegdixon@yahoo.com>
2019-10-02 14:23:45 +0100

    Exercise 3

--- note-list-view.js
+++ note-list-view.js
@@ -1,4 +1,17 @@
-(function NoteListView(exports){
-  
+(function (exports) {
+  function NoteListView(noteList) {
+    this.noteList = noteList
+  }
+
+  NoteListView.prototype.returnsView = function () {
+    let output = '<ul>'
+    this.noteList.list.forEach(function (note) {
+      output += `<li><div>${note.text}</div></li>`
+    })
+    output += '</ul>'
+    console.log(output)
+    return output
+  }
   exports.NoteListView = NoteListView
 })(this)
+
